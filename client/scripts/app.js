8fae2a17981b72d012e72b3dfd7e5ef59ab0fdf1
LisaALarson <lisalarsonll@gmail.com>
2015-09-11 16:09:49 -0500

    room list added

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,84 @@
 console.log("this is a console log");
+
+var roomArray = [
+    {
+        roomNumber:2161,
+        capacity: 10,
+        computer: true
+    },
+    {
+        roomNumber:3212,
+        capacity: 10,
+        computer: true
+    },
+    {
+        roomNumber:3487,
+        capacity: 4,
+        computer: false
+    },
+    {
+        roomNumber:4112,
+        capacity: 4,
+        computer: false
+    },
+    {
+        roomNumber:4141,
+        capacity: 4,
+        computer: false
+    },
+    {
+        roomNumber:4181,
+        capacity: 8,
+        computer: true
+    },
+    {
+        roomNumber:4211,
+        capacity: 4,
+        computer: false
+    },
+    {
+        roomNumber:4245,
+        capacity: 111,
+        computer: "uknown"
+    },
+    {
+        roomNumber:4261,
+        capacity: 4,
+        computer: false
+    },
+    {
+        roomNumber:4312,
+        capacity: 6,
+        computer: false
+    },{
+        roomNumber:4416,
+        capacity: 8,
+        computer: false
+    },
+    {
+        roomNumber:4487,
+        capacity: 6,
+        computer: false
+    },
+    {
+        roomNumber:4541,
+        capacity: 10,
+        computer: true
+    },
+    {
+        roomNumber:"The Boardroom",
+        capacity: 36,
+        computer: true
+    },
+    {
+        roomNumber:"The Street Space",
+        capacity: "unknown",
+        computer: "unknown"
+    },
+    {
+        roomNumber:"Training Room",
+        capacity: 16,
+        computer: true
+    },
+
+];
