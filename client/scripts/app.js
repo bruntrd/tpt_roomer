ce8b2520b36a4a502f8fe305569549326f60b826
LisaALarson <lisalarsonll@gmail.com>
2015-09-14 16:10:57 -0500

    adds time functionality

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,4 +1,112 @@
 console.log("this is a console log");
+//variables
+var currentTime = new Date();
+var minutes = currentTime.getMinutes();
+var thirtyTime;
+var sixtyTime;
+var timeArray = [];
+
+//functions
+function buttonTime(){
+    timeArray = [];
+    if(minutes >= 45){
+        thirtyTime = currentTime.getHours() + 1 + ":30";
+        sixtyTime = currentTime.getHours() + 2 + ":00";
+        if(parseInt(thirtyTime) > 12){
+            thirtyTime = parseInt(thirtyTime) - 12 + ":30";
+        }
+        if(parseInt(sixtyTime) > 12){
+            sixtyTime = parseInt(sixtyTime) - 12 + ":00";
+        }
+        console.log("Thirty Time: " + thirtyTime);
+        console.log("Sixty Time: " + sixtyTime);
+    }
+    else if(minutes >= 30 && minutes < 45){
+        thirtyTime = currentTime.getHours() + 1 + ":00";
+        sixtyTime = currentTime.getHours() + 1 + ":30";
+        if(parseInt(thirtyTime) > 12){
+            thirtyTime = parseInt(thirtyTime) - 12 + ":00";
+        }
+        if(parseInt(sixtyTime) > 12){
+            sixtyTime = parseInt(sixtyTime) - 12 + ":30";
+        }
+        console.log("Thirty Time: " + thirtyTime);
+        console.log("Sixty Time: " + sixtyTime);
+    }
+    else if(minutes >= 15 && minutes < 30){
+        thirtyTime = currentTime.getHours() + 1 + ":00";
+        sixtyTime = currentTime.getHours() + 1 + ":30";
+        if(parseInt(thirtyTime) > 12){
+            thirtyTime = parseInt(thirtyTime) - 12 + ":00";
+        }
+        if(parseInt(sixtyTime) > 12){
+            sixtyTime = parseInt(sixtyTime) - 12 + ":30";
+        }
+        console.log("Thirty Time: " + thirtyTime);
+        console.log("Sixty Time: " + sixtyTime);
+    }
+    else if(minutes <15 &&  minutes >= 0){
+        thirtyTime = currentTime.getHours() + ":30";
+        sixtyTime = currentTime.getHours() + 1 + ":00";
+        if(parseInt(thirtyTime) > 12){
+            thirtyTime = parseInt(thirtyTime) - 12 + ":30";
+        }
+        if(parseInt(sixtyTime) > 12){
+            sixtyTime = parseInt(sixtyTime) - 12 + ":00";
+        }
+        console.log("Thirty Time: " + thirtyTime);
+        console.log("Sixty Time: " + sixtyTime);
+    }
+    timeArray.push(thirtyTime);
+    timeArray.push(sixtyTime);
+    console.log(timeArray);
+}
+
+
+function ajaxCall(){
+    $.ajax({
+        url:knjnkm , //need from cat and ryan
+        success: function(response){
+            appendInfo(response);
+        },
+        error: function(request, errorType, errorMessage){
+            alert(errorType + " " + errorMessage);
+        }
+    });
+}
+
+function appendInfo(){
+    buttonTime();
+    for(var i = 0; i<roomArray.length; i++){
+        $('#rooms').append("<div><p>" + roomArray[i].roomNumber + "</p><button>" + thirtyTime + "</button><button>" + sixtyTime + "</button></div>");
+    }
+    //$.each(response, function(index, response){
+    //    //$('#rooms').append("<div><button>" + thirtyTime + "</button><button>" + sixtyTime + "</button></div>");
+    //});
+}
+
+
+$(document).ready(function(){
+
+    buttonTime();
+    appendInfo();
+//console.log(new Date().getTime());
+    //$('#rooms').append("<div><button>" + thirtyTime + "</button><button>" + sixtyTime + "</button></div>");
+
+}); // end document ready
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 var roomArray = [
     {
