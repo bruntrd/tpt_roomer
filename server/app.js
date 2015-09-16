25dfaad74d820fea02fa57d5d594846b24b53812
jamesthe500 <jamesddaniels@gmail.com>
2015-09-16 11:18:43 -0500

    adjusts server app.js to work with heroku hopfully

--- server/app.js
+++ server/app.js
@@ -3,13 +3,16 @@ var app = express();
 var index = require('./routes/index');
 
 
+app.set("port", process.env.PORT || 3000);
 
-
-var server = app.listen(3000, function(){
+/*var server = app.listen(3000, function(){
     var port=server.address().port;
     console.log("Listening on port: ", port);
-});
+});*/
 
+app.listen(app.get("port"), function(){
+    console.log("listening on 3000")
+});
 
 app.use("/", index);
 
